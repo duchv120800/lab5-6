09fae99accbae6fcb4f4756a1070fc37f7a60ae4
Hoang Duc <duchv1208@gmail.com>
2024-11-22 10:22:25 +0700

    add lab5 6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MovieController::class, 'index'])->name('index');
+
+Route::get('/show/{id}', [MovieController::class, 'show'])->name('show');
+
+Route::get('/create', [MovieController::class, 'create'])->name('create');
+
+Route::post('/create', [MovieController::class, 'store'])->name('store');
+
+Route::get('/edit/{id}', [MovieController::class, 'edit'])->name('edit');
+
+Route::put('/edit/{id}', [MovieController::class, 'update'])->name('update');
+
+Route::delete('/delete/{id}', [MovieController::class, 'destroy'])->name('destroy');
+
+Route::get('/trash', [MovieController::class, 'trash'])->name('trash');
+
+Route::get('/search', [MovieController::class, 'search'])->name('search');
